c9dcbf8af352c2d4577db34bdce0833b461ceb32
you0626 <sy0626sk@icloud.com>
2024-09-18 23:07:56 +0900

    バリテーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,18 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :email, uniqueness: true,
-                      format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'must contain @ and be a valid email address' }
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers' }
+                      format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
     validates :first_name,
-              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters (kanji, hiragana, katakana)' }
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
     validates :last_name,
-              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters (kanji, hiragana, katakana)' }
-    validates :first_name_katakana, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
-    validates :last_name_katakana, format: { with: /\A[ァ-ヶー]+\z/, message: 'must be full-width katakana characters' }
+              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' }
+    validates :first_name_katakana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
+    validates :last_name_katakana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' }
     validates :birth
   end
 
-  has_many :prototypes
-  has_many :comments
+  has_many :items
+  has_many :orders
 end
