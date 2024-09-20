bfc7637f8dc5611ebb451d1534ba596d2160c7b4
you0626 <sy0626sk@icloud.com>
2024-09-20 23:14:50 +0900

    単品テスト

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品の出品' do
+    context '商品が出品できる場合' do
+      it 'すべての項目が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品が出品できない場合' do
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'titleが空では出品できない' do
+        @item.title = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'titleが40文字を超えると出品できない' do
+        @item.title = 'a' * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Title is too long (maximum is 40 characters)')
+      end
+
+      it 'explanationが空では出品できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it 'explanationが1000文字を超えると出品できない' do
+        @item.explanation = 'a' * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Explanation is too long (maximum is 1000 characters)')
+      end
+
+      it 'category_idが0では出品できない' do
+        @item.category_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it 'state_idが0では出品できない' do
+        @item.state_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("State can't be blank")
+      end
+
+      it 'spirit_idが0では出品できない' do
+        @item.spirit_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Spirit can't be blank")
+      end
+
+      it 'prefecture_idが0では出品できない' do
+        @item.prefecture_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'day_idが0では出品できない' do
+        @item.day_id = 0
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Day can't be blank")
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it 'priceが300未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it 'priceが9,999,999を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+
+      it 'priceが数値でないと出品できない' do
+        @item.price = 'abc'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+
+      it 'ユーザーが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,7 @@ RSpec.configure do |config|
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
+    config.include FactoryBot::Syntax::Methods
   end
 
   config.before(:each) do
