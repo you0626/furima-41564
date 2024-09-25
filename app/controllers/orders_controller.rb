f6a2e2667122af4bf7e168ec18d6817cee2215a7
you0626 <sy0626sk@icloud.com>
2024-09-25 19:19:15 +0900

    PAY.jp導入

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,19 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
+      Payjp.api_key = 'sk_test_0a9108245e41d108c35c16e9'
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: @order_address.token,
+        currency: 'jpy'
+      )
       @order_address.save
       redirect_to root_path
     else
@@ -19,6 +26,10 @@ class OrdersController < ApplicationController
 
   def order_address_params
     params.require(:order_address).permit(:postal_code, :prefecture_id, :municipality, :house_number, :building_name,
-                                          :telephone_number, :user_id, :item_id)
+                                          :telephone_number, :user_id, :item_id).merge(token: params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,7 +1,8 @@
 class OrderAddress
   include ActiveModel::Model
 
-  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :user_id,
+                :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
